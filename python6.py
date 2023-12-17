76e2fde8d0a154e700992e9f54488bcba7811437
THARUN ANAND <103479706+THARUN1526@users.noreply.github.com>
2023-12-17 14:03:45 +0530

    Add files via upload

---
+++ python6.py
@@ -0,0 +1,5 @@
+figure = px.scatter(data_frame = data, x="Number Of Ratings",
+                    y="Sale Price", size="Discount Percentage", 
+                    trendline="ols", 
+                    title="Relationship between Sale Price and Number of Ratings of iPhones")
+figure.show()
